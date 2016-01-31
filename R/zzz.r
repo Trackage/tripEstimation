9bf22abf34dc295d1355bb58573c248564d38383
Michael Sumner <mdsumner@gmail.com>
2016-01-31 14:11:26 +1100

    maintenance

---
+++ R/zzz.r
@@ -0,0 +1,4 @@
+.onLoad <- function(libname, pkgname) {
+  message("tripEstimation has been superseded by SGAT, please look at using that package instead:\nhttps://github.com/SWotherspoon/SGAT")
+  invisible()
+}
